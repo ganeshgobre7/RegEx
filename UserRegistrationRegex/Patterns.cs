edc37dc50f31967bd55fd0770425998a845644ab
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 18:15:51 +0530

    Validate First Name

---
+++ UserRegistrationRegex/Patterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    class Patterns
+    {
+       public static string reg_pattern = "^[A-Z][a-z]{3,}";
+        public bool firstName( string fName)
+        {
+            return Regex.IsMatch(fName, reg_pattern);
+        }
+    }
+}
--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -7,6 +7,29 @@ namespace UserRegistrationRegex
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE USER REGISTRATION REGEX PROBLEM");
+
+            Patterns patterns = new Patterns();
+            int defCount = 0;
+            while (defCount == 0)
+            {
+                Console.WriteLine("1.First Name 2.Exit");
+                Console.WriteLine("Choose Your Option");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+
+                        Console.Write("Insert the First Name :");
+                        string str = Console.ReadLine();
+                        Console.WriteLine(patterns.firstName(str));
+                        break;
+                    default:
+                        Console.WriteLine("End");
+                        defCount++;
+                        break;
+                }
+            }
+
         }
     }
 }
