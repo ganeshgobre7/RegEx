201ce4d06f195b97cd813e6af04ed6c1eb8f4839
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 18:36:55 +0530

    Validate Last Name

--- UserRegistrationRegex/Patterns.cs
+++ UserRegistrationRegex/Patterns.cs
@@ -7,10 +7,26 @@ namespace UserRegistrationRegex
 {
     class Patterns
     {
-       public static string reg_pattern = "^[A-Z][a-z]{3,}";
+        public static string reg_pattern = "^[A-Z][a-z]{3,}";
+            /// <summary>
+            /// Method to Validate the First Name
+            /// </summary>
+            /// <param name="fName"></param>
+            /// <returns></returns>
         public bool firstName( string fName)
         {
             return Regex.IsMatch(fName, reg_pattern);
+        }
+
+        public static string reg_pattern1 = "^[A-Z][a-z]{3,}";
+        /// <summary>
+        /// Method to Validate the Last Name
+        /// </summary>
+        /// <param name="lName"></param>
+        /// <returns></returns>
+        public bool lastName(string lName)
+        {
+            return Regex.IsMatch(lName, reg_pattern1);
         }
     }
 }
--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -12,16 +12,20 @@ namespace UserRegistrationRegex
             int defCount = 0;
             while (defCount == 0)
             {
-                Console.WriteLine("1.First Name 2.Exit");
+                Console.WriteLine("1.First Name 2.Last Name 3.Exit");
                 Console.WriteLine("Choose Your Option");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-
                         Console.Write("Insert the First Name :");
                         string str = Console.ReadLine();
                         Console.WriteLine(patterns.firstName(str));
+                        break;
+                    case 2:
+                        Console.Write("Insert the Last Name :");
+                        string str1 = Console.ReadLine();
+                        Console.WriteLine(patterns.lastName(str1));
                         break;
                     default:
                         Console.WriteLine("End");
