c9497886988f9349e1c71ce3e32c4e6255afb485
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 17:24:48 +0530

    Welcomd message added

---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE USER REGISTRATION REGEX PROBLEM");
+        }
+    }
+}
