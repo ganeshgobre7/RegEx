f6307e4240f5cac00ab1f9cc109ac3c867b3354c
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-28 12:16:22 +0530

    Validating password Rule 3

--- UserRegistrationRegex/Patterns.cs
+++ UserRegistrationRegex/Patterns.cs
@@ -29,7 +29,7 @@ namespace UserRegistrationRegex
             return Regex.IsMatch(lName, reg_pattern1);
         }
 
-        public static string reg_pattern2= @"^[a-z0-9]+[@]+[a-z]+[.][a-z]{2,3}";
+        public static string reg_pattern2= @"^[a-z0-9]+[@]+[a-z]+[.][a-z]{2,3}$";
         /// <summary>
         /// Method to Validate the Email Id
         /// </summary>
@@ -39,7 +39,7 @@ namespace UserRegistrationRegex
         {
             return Regex.IsMatch(email, reg_pattern2);
         }
-        public static string reg_pattern3= @"^[+]{0,1}[0-9]{2}" + " " + "[6-9]{1}[0-9]{9}";
+        public static string reg_pattern3= @"^[+]{0,1}[0-9]{2}" + " " + "[6-9]{1}[0-9]{9}$";
         /// <summary>
         /// Method to validate the Mobile Number
         /// </summary>
@@ -50,7 +50,7 @@ namespace UserRegistrationRegex
             return Regex.IsMatch(mobileNo, reg_pattern3);
         }
 
-        public static string reg_pattern_4 = @"(?=.*[A-Z])[a-zA-Z]{8,}";
+        public static string reg_pattern_4 = @"^(?=.*[A-Z])[a-zA-Z0-9]{8}$";
         /// <summary>
         /// Method to validate the password
         /// </summary>
